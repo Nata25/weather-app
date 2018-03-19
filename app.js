2bb563e2e04b60c7a8b98cfafc4b7f466cf4e97a
Natalya Ivanova <natalyas.ivanova@gmail.com>
2018-03-19 14:06:41 +0200

    Code refactoring

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode.js');
 
-const key = 'AIzaSyC1Snx7sxW_urBAP72fC1WUgPWbAJqjvgs';
 let address;
 const argv = yargs
 .options({
@@ -16,22 +15,11 @@ const argv = yargs
 .alias('help', 'h')
 .argv;
 
-address = encodeURIComponent(argv.address);
-// can be smth like 14108 Natalii Uzhvii Street 10 Kyiv Ukraine'
-
-request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address +
-  "&key=" + key,
-  json: true,
-}, (error, response, body) => {
-  // console.log(JSON.stringify(body, undefined, 2));
-  if (error) {
-    console.log('Unable to connect to Google service');
-  } else if (body.status === 'ZERO_RESULTS') {
-    console.log('Unable to find the address');
-  } else if (body.status === 'OK') {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
+// address can be smth like 14108 Natalii Uzhvii Street 10 Kyiv Ukraine'
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(results, undefined, 2));
   }
 });
---
+++ geocode/geocode.js
@@ -0,0 +1,27 @@
+const request = require('request');
+const key = 'AIzaSyC1Snx7sxW_urBAP72fC1WUgPWbAJqjvgs';
+
+module.exports.geocodeAddress = (rawAddress, callback) => {
+  const address = encodeURIComponent(rawAddress);
+  request({
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address +
+    "&key=" + key,
+    json: true,
+  }, (error, response, body) => {
+    // console.log(JSON.stringify(body, undefined, 2));
+    if (error) {
+      callback('Unable to connect to Google service');
+    } else if (body.status === 'ZERO_RESULTS') {
+      callback('Unable to find the address');
+    } else if (body.status === 'OK') {
+      callback(undefined, {
+        address: body.results[0].formatted_address,
+        latitude: body.results[0].geometry.location.lat,
+        longitude: body.results[0].geometry.location.lng,
+      });
+      // console.log(`Address: ${body.results[0].formatted_address}`);
+      // console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+      // console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
+    }
+  });
+}
